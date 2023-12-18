c037eba5f65b7a16d6b8ea47d568364ae67d53fb
Maxwell <njoromax5@gmail.com>
2023-12-18 23:03:15 +0300

    Code challenge

---
+++ lib/Reviews.py
@@ -0,0 +1,15 @@
+class Review:
+    reviews = []
+
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+        Review.reviews.append(self)
+
+    def rating(self):
+        return self.rating
+
+    @classmethod
+    def all(cls):
+        return cls.reviews
